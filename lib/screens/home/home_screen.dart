215e2b0f4a9d0df3b283de1240aee35b3f4d531d
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-02-15 16:27:02 +0800

    MInor ui changes

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -17,112 +17,132 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: 30.h),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 30.h),
-              Row(
-                children: [
-                  const CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/avatar.jpeg'),
-                  ),
-                  SizedBox(width: 5.w),
-                  const CircleAvatar(
-                    backgroundColor: AppColors.accentColor,
-                    child: Icon(
-                      Icons.notifications,
-                      color: AppColors.fontWhite,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.w),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 30.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const CircleAvatar(
+                              backgroundImage:
+                                  AssetImage('assets/images/avatar.jpeg'),
+                            ),
+                            SizedBox(width: 10.w),
+                            Text(
+                              'John Doe',
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 20.sp,
+                                color: AppColors.fontWhite
+                              ),
+                            ),
+                          ],
+                        ),
+                        const CircleAvatar(
+                          backgroundColor: AppColors.accentColor,
+                          child: Icon(
+                            Icons.notifications,
+                            color: AppColors.fontWhite,
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 30.h),
-              Text(
-                'Total Balance',
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 20.sp,
-                  color: AppColors.fontLight,
+                    SizedBox(height: 30.h),
+                    Text(
+                      'Total Balance',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 20.sp,
+                        color: AppColors.fontLight,
+                      ),
+                    ),
+                    Text(
+                      '\$20000',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.fontWhite,
+                      ),
+                    ),
+                    SizedBox(height: 50.h),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AnalyticsText(
+                          titleText: 'Total Spent',
+                          amount: 1000,
+                        ),
+                        AnalyticsText(
+                          titleText: 'Total Income',
+                          amount: 2000,
+                        ),
+                        AnalyticsText(
+                          titleText: 'Goal',
+                          amount: 15000,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30.h),
+                    Row(
+                      children: [
+                        ActionButtonContainer(
+                          onpressed: () {},
+                          title: 'Add Expense',
+                          icon: Icons.credit_card,
+                        ),
+                        SizedBox(width: 5.w),
+                        ActionButtonContainer(
+                          onpressed: () {},
+                          title: 'Add Income',
+                          icon: Icons.add_card,
+                        ),
+                        SizedBox(width: 5.w),
+                        CircleAvatar(
+                          radius: 30.r,
+                          child: const Icon(Icons.more_horiz_rounded),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 30.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Recent Activities',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 20.sp,
+                            color: AppColors.fontWhite,
+                          ),
+                        ),
+                        Text(
+                          'view all',
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 18.sp,
+                            color: AppColors.fontLight,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 20.h),
-              Text(
-                '\$20000',
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 30.sp,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.fontWhite,
-                ),
-              ),
-              SizedBox(height: 20.h),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  AnalyticsText(
-                    titleText: 'Total Spent',
-                    amount: 1000,
-                  ),
-                  AnalyticsText(
-                    titleText: 'Total Income',
-                    amount: 2000,
-                  ),
-                  AnalyticsText(
-                    titleText: 'Goal',
-                    amount: 15000,
-                  ),
-                ],
-              ),
-              SizedBox(height: 30.h),
-              Row(
-                children: [
-                  ActionButtonContainer(
-                    onpressed: () {},
-                    title: 'Add Expense',
-                    icon: Icons.credit_card,
-                  ),
-                  SizedBox(width: 5.w),
-                  ActionButtonContainer(
-                    onpressed: () {},
-                    title: 'Add Income',
-                    icon: Icons.add_card,
-                  ),
-                  SizedBox(width: 5.w),
-                  CircleAvatar(
-                    radius: 30.r,
-                    child: const Icon(Icons.more_horiz_rounded),
-                  )
-                ],
-              ),
-              SizedBox(height: 20.h),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Recent Activities',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 20.sp,
-                      color: AppColors.fontWhite,
-                    ),
-                  ),
-                  Text(
-                    'view all',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 18.sp,
-                      color: AppColors.fontLight,
-                    ),
-                  )
-                ],
               ),
               SizedBox(height: 10.h),
               Container(
-                height: 500.h,
+                height: 370.h,
                 decoration: BoxDecoration(
                   color: AppColors.fontWhite,
                   borderRadius: BorderRadius.only(
