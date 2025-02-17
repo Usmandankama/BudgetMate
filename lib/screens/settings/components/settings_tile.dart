63a7680b93e4605ff790c5ddf5f419bebc7f6bcd
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-02-18 00:43:12 +0800

    Create settings_tile.dart

---
+++ lib/screens/settings/components/settings_tile.dart
@@ -0,0 +1,46 @@
+import 'package:budget_mate/constatnts/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SettingsTile extends StatelessWidget {
+  const SettingsTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(5.sp),
+      height: 70.h,
+      decoration: BoxDecoration(
+        color: AppColors.accentColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.r),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Manage accounts',
+                  style: TextStyle(
+                    fontSize: 15.sp,
+                    fontFamily: 'Poppins',
+                    color: AppColors.fontWhite,
+                  ),
+                ),
+              ],
+            ),
+            const Icon(
+              Icons.keyboard_arrow_right_rounded,
+              color: AppColors.fontWhite,
+              size: 30,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
