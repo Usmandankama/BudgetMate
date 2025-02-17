ba33032490d3da6cfc93773ada782be84f3b0607
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-02-18 00:17:18 +0800

    Update settings_screen.dart

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,4 +1,6 @@
+import 'package:budget_mate/constatnts/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -10,6 +12,26 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppColors.primaryColor,
+        title: Text(
+          'Settings',
+          style: TextStyle(
+            fontSize: 24.sp,
+            color: AppColors.fontWhite,
+          ),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ],
+        ),
+      ),
+    );
   }
 }
