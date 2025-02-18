06015911b9636f7b27e1e02dc5626aaadca71c78
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-02-19 00:28:08 +0800

    Create expense_screen.dart

---
+++ lib/screens/expenses/expense_screen.dart
@@ -0,0 +1,18 @@
+import 'package:budget_mate/constatnts/colors.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseScreen extends StatefulWidget {
+  const ExpenseScreen({super.key});
+
+  @override
+  State<ExpenseScreen> createState() => _ExpenseScreenState();
+}
+
+class _ExpenseScreenState extends State<ExpenseScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+    );
+  }
+}
