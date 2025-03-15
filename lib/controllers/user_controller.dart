db574fec36527f20aed3aebb1d272e9017394c6b
Usman Abubakar <104586608+Usmandankama@users.noreply.github.com>
2025-03-16 01:21:23 +0800

    Update user_controller.dart

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 
 class UserController extends GetxController {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final GetStorage _storage = GetStorage();
 
   RxMap userData = {}.obs;
 
@@ -22,17 +20,9 @@ class UserController extends GetxController {
       if (user != null) {
         DocumentSnapshot userDoc = await _firestore.collection('users').doc(user.uid).get();
         userData.value = userDoc.data() as Map<String, dynamic>;
-
-        // Store in local storage
-        _storage.write("userData", userData);
       }
     } catch (e) {
       Get.snackbar("Error", "Failed to fetch user data");
     }
-  }
-
-  void clearUserData() {
-    userData.value = {};
-    _storage.remove("userData");
   }
 }
